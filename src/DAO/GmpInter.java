dc8bdd394f881e22a00b9317a9060b1ab2989983
Abhinandan Rauniyar <103803319+rakeshrauniyar12@users.noreply.github.com>
2022-12-16 22:01:45 +0530

    Update GmpInter.java

--- src/DAO/GmpInter.java
+++ src/DAO/GmpInter.java
@@ -2,12 +2,13 @@ package DAO;
 
 import java.util.List;
 
+import DTO.EmpDTO;
 import Exception.GmpException;
 import Model.Employee;
 
 public interface GmpInter {
-       public String createEmployee(int eid,String ename,String address,long mbl,int days,int wages,int gid) throws GmpException;
+   public String createEmployee(int eid,String ename,String address,long mbl,int days,int wages,int gid,int pid) throws GmpException;
        public List<Employee> viewAlDetailsOfEmp() throws GmpException;
-       public String assignEmpToProject(int pid) throws GmpException;
-       public List<Employee> viewAllWagesAndDays() throws GmpException;
+       public String assignEmpToProject(int pid,int eid) throws GmpException;
+       public List<EmpDTO> viewAllWagesAndDays(int pid) throws GmpException;
 }
